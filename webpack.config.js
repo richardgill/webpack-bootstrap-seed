865533c8ea221d7bd9bae4df4bd8f95abfdf7dd6
Richard Gill <richard@rgill.co.uk>
2015-10-25 19:07:52 +0000

    Removed unused comments

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,6 @@ module.exports = {
         test: /\.scss$/,
         loader: 'style!css!sass'
       },
-      // the url-loader uses DataUrls.
-      // the file-loader emits files.
       {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
