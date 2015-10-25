937320fc1935ce8eba825631b1f1308b1dfed859
Richard Gill <richard@rgill.co.uk>
2015-10-25 16:31:43 +0000

    Bootstrap added to project

--- webpack.config.js
+++ webpack.config.js
@@ -11,15 +11,23 @@ module.exports = {
       filename: 'bundle.js',
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    },
-    {
-      test: /\.scss$/,
-      loader: 'style!css!sass'
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      },
+      // the url-loader uses DataUrls.
+      // the file-loader emits files.
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
   }
 
 };
